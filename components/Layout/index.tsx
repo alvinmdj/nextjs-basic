2d191d108b0fa29a715b0ab2e4afa293a8ec1343
AlvinMD <alvinmartindwrk@gmail.com>
2021-12-21 21:30:57 +0700

    fix footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,6 +3,7 @@ import Footer from '../Footer'
 import { ReactNode } from 'react'
 import { Container } from "@chakra-ui/react"
 import Head from 'next/head'
+import styles from './Layout.module.css'
 
 interface LayoutProps {
   children: ReactNode
@@ -13,14 +14,16 @@ export default function Layout(props: LayoutProps) {
   const { children, pageTitle } = props
 
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Next.js Basic | { ' ' } { pageTitle }</title>
         <meta name="description" content="Next.js basic web application" />
       </Head>
       <Header />
-      <Container height="80%" maxW='container.xl' px={10} mt={5}>{ children }</Container>
-      <Footer />      
-    </>
+      <Container className={styles.content} maxW='container.xl' px={10} my={5}>
+        { children }
+      </Container>
+      <Footer />
+    </div>
   )
 }
